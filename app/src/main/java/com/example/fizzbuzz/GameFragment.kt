2911020f2a8fe1c6c180ed3902516f63cdcd4f47
osmanhadzic <hadzico39@gmail.com>
2020-09-18 11:02:23 +0200

    navigation

---
+++ app/src/main/java/com/example/fizzbuzz/GameFragment.kt
@@ -0,0 +1,21 @@
+package com.example.fizzbuzz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameFragment : Fragment(R.layout.fragment_game) {
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/fizzbuzz/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.fizzbuzz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_abut_app2.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
